74fde594fcd83df08e0c135a89e65a37614c05eb
Kiran Subash <kiran.subash@gmail.com>
2016-07-28 18:27:32 +0530

    Update fpm-python.sh

--- fpm-python.sh
+++ fpm-python.sh
@@ -1,8 +1,11 @@
 # Create CentOS 6 RPM package of Python 2.7.6.
 # Requires: fpm
 
-mkdir /tmp/fpm-python ; mkdir /tmp/fpm-python-installdir
-cd /tmp/fpm-python
+DOWNLOAD_DIR=/tmp/fpm-python
+TMP_INSTALL_DIR=/tmp/fpm-python-installdir
+
+mkdir ${DOWNLOAD_DIR} ; mkdir ${TMP_INSTALL_DIR}
+cd ${DOWNLOAD_DIR}
 curl -LO http://python.org/ftp/python/2.7.6/Python-2.7.6.tgz && \
   tar xf Python-2.7.6.tgz && \
   cd Python-2.7.6
@@ -18,6 +21,29 @@ yum -y install \
   expat-devel
 
 ./configure --prefix=/usr/local --enable-unicode=ucs4 --enable-shared --enable-ipv6
+make -j2
+
+make install DESTDIR=${TMP_INSTALL_DIR}
+
+echo '/sbin/ldconfig' > ${TMP_INSTALL_DIR}/run-ldconfig.sh
+
+fpm -s dir -t rpm -n python27 -v 2.7.6 -C ${TMP_INSTALL_DIR} \
+  --after-install ${TMP_INSTALL_DIR}/run-ldconfig.sh \
+  -d 'openssl' \
+  -d 'bzip2' \
+  -d 'zlib' \
+  -d 'expat' \
+  -d 'db4' \
+  -d 'sqlite' \
+  -d 'ncurses' \
+  -d 'readline' \
+  --directories=/usr/local/lib/python2.7/ \
+  --directories=/usr/local/include/python2.7/ \
+  usr/local
+
+
+
+
 
 
 
