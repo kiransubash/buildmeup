a74ddf7cb9da9167745eab4ac5e937da3b8b0942
Kiran Subash <kiran.subash@gmail.com>
2020-03-23 14:45:10 +0530

    Create install_docker_centos7.sh

---
+++ install_docker_centos7.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+sudo yum-config-manager -y --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+sudo systemctl enable docker
+sudo systemctl start docker
